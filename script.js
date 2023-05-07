cbb13db927765de24a0b8ad6c8a28b4c3c968835
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-05-07 00:09:35 -0500

    all the buttons are now functioning

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('our journey begins');
-    let submitButton = $('#submit-btn').val();
+    //let submitButton = $('#submit-btn').val();
+    //$('#submit-btn').on('click',submitHandle);
     $('#form-input').on('submit', submitHandle);
     $('#table-input').on('click','.delete-btn',handleDelete);
+    
 
 }
-
+let MonthlySalary = 0;
 function submitHandle(event) {
+console.log('insubmit');
     let nameInput = $('#name-input').val();
     let lastnameInput = $('#lastname-input').val();
     let idInput = $('#id-input').val();
     let titleInput = $('#title-input').val();
-    let salaryInput = $('#salary-input');
+    let salaryInput = $('#salary-input').val();
+    
     event.preventDefault();
     console.log('name:',nameInput,'lastname:',lastnameInput,'id',idInput,'title:',titleInput,'Annual Salary:',salaryInput);
 
-    $('#table-input').append(`<tr><td>${nameInput}</td><td>${lastnameInput}</td><td>${idInput}</td><td>${titleInput}</td><td><${salaryInput}/td><td><button class="delete-btn">Delete</button></td></tr>`);
-
+    $('#table-input').append(`<tr><td>${nameInput}</td><td>${lastnameInput}</td><td>${idInput}</td><td>${titleInput}</td><td>${salaryInput}</td><td><button class="delete-btn">Delete</button></td></tr>`);
+    let MonthlySalary = (salaryInput/12);
+    MonthlySalary++;
+    $('#total-monthly').text(MonthlySalary);
 }
 function handleDelete() {
     $(this).parent().parent().remove();
     
     
     }
+
